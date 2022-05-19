06505cca00b3a8cee04899879269b6c53fb83882
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-19 22:35:14 +0900

    update app.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/analytics';  
 
 // import firebase hooks
 import { useAuthState } from 'react-firebase-hooks';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { signOut } from 'firebase/auth';
 
 // 作成したfirebaseのアプリのコンフィグを入れる
 firebase.initializeApp({
@@ -54,5 +56,58 @@ function Signin() {
   )
 }
 
+// サインアウト機能
+function signOut() {
+  return auth.currentUser && (
+
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+// chatroomを描画する機能
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
+}
 
 export default App;
