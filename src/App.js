905f3b6aa0a25538dea307baecd8c2543e87c9ab
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-19 22:35:41 +0900

    update app.js

--- src/App.js
+++ src/App.js
@@ -110,4 +110,17 @@ function ChatRoom() {
   </>)
 }
 
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
+}
+
 export default App;
